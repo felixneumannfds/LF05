c4d702a4876151dea1bcb2ebe1a36b8107cf611e
felixneumannfds <145192498+felixneumannfds@users.noreply.github.com>
2023-11-12 10:13:45 +0000

    main.py

--- GrundlageSeite05/Aufgabe01/main.py
+++ GrundlageSeite05/Aufgabe01/main.py
@@ -1,24 +1,27 @@
 import math
-# H = Höhe
-# v = Fallgeschwindigkeit in m/s
+from tabulate import tabulate
 
-# h in m
-h = int(input('h: '))
-print(h, 'm Höhe')
+# Erste Reihe der Tabelle
+pq_table = [["h in m", "v in m/s", "v in km/h"]]
 
-# v in m/s
-v = math.sqrt(2 * 9.81 * h)
+# Benutzereingabe für maximale Höhe und Schrittweite
+max_height = float(input("Geben Sie die maximale Höhe in Metern ein: "))
+step = float(input("Geben Sie die Schrittweite für die Höhe ein: "))
 
-# float to double
-v_rounded = round(v, 2)
-print(v_rounded, 'm/s', 'Fallgeschwindigkeit')
-
-# v in km/h
-kmh = v * 3.6
-
-# float to double
-kmh_rounded = round(kmh, 2)
-print(kmh_rounded, 'km/h', 'Fallgeschwindkeit')
+# Schleife, um die Tabelle zu füllen
+h = 0
+while h <= max_height:
+    v_mps = math.sqrt(2 * 9.8 * h)  # Berechnung für v in m/s
+    v_kmph = v_mps * 3.6  # Umrechnung von m/s in km/h
+    # Runden auf zwei Dezimalstellen
+    h = round(h, 2)
+    v_mps = round(v_mps, 2)
+    v_kmph = round(v_kmph, 2)
+    new_data = [h, v_mps, v_kmph]
+    pq_table.append(new_data)
+    h += step
+# Die Tabelle drucken
+print(tabulate(pq_table, headers='firstrow', tablefmt='fancy_grid'))
 
 
 
