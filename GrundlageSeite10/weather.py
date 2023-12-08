1cc0c2c026e5ba4cfdde800ce64213b431b5a0a3
felixneumannfds <145192498+felixneumannfds@users.noreply.github.com>
2023-12-08 16:40:37 +0100

    weather.py

--- GrundlageSeite10/weather.py
+++ GrundlageSeite10/weather.py
@@ -16,7 +16,8 @@ user_hour = int(input("Stunde eingeben: "))
 print("Um", user_hour, ":00 Uhr waren es: ", temps[user_hour], "°C")
 
 # 1.3
-
+average_temp = sum(temps) / len(temps)
+print("Durchschnittstemperatur:", average_temp, "°C")
 temps.sort(reverse=True)
 highest_temp = temps[0]
 lowest_temp = temps[23]
@@ -24,6 +25,7 @@ lowest_temp = temps[23]
 print("Höchsttemperatur :", highest_temp, "°C")
 print("Tiefstemperatur :", lowest_temp, "°C")
 
+# 1.4
 if highest_temp < 0 and lowest_temp < 0:
     print("Eistag")
 
